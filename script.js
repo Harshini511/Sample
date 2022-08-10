7c4e5ba998a8c472dfbd4428294955c92a2485b4
Harshini S <75244875+Harshini511@users.noreply.github.com>
2022-08-10 08:14:29 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form");
+       // nextBtn = form.querySelector(".nextBtn"),
+        //allInput = form.querySelectorAll(".first input");
+
+
+        function update() {
+            var select = document.getElementById('entity');
+            var entity = select.options[select.selectedIndex].value;
+            
+            var time = document.getElementById("time").value;
+
+            var date = document.getElementById("date").value;
+            
+            var quantity = document.getElementById("quantity").value;
+            
+            var price = document.getElementById("price").value;
+            
+            var select = document.getElementById('bs');
+            var bs = select.options[select.selectedIndex].value;
+
+            //window.location.assign("http://localhost:8080/welcome/"+time);
+            window.location.assign("http://localhost:8080/");
+        }
+
+        function reset()
+        {
+        document.getElementById("form").reset();
+        }
+
+
